6da16349507f69de414671c9c9fcc0eabc0293f3
Joël Charles <joel.charles91@gmail.com>
2012-12-13 14:31:25 +0100

    Fix: Albumart not showing

--- ajax/getAlbumArt.php
+++ ajax/getAlbumArt.php
@@ -6,7 +6,7 @@ require_once "../app/include.php";
 require_once "../app/utils.php";
 //header('Content-Type: text/html; charset=UTF-8');
 if (isset($_POST['albumpath'])){
-	$albumpath = urldecode($_POST['albumpath']);
+	$albumpath = $_POST['albumpath'];
 	$a = array();
 	$names = array('Folder.jpg', 'folder.jpg');
 	foreach($names as $name){
