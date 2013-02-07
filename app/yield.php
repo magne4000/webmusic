d2474a81b3d2f2fe3b8afc67b682271f5b2cfe13
Joël Charles <joel.charles91@gmail.com>
2013-02-07 15:46:22 +0100

    Yield workaround for chrome (which have a empty responseText until x kb are sent by default)

--- app/yield.php
+++ app/yield.php
@@ -6,6 +6,9 @@ function yield($data){
     if (!isset($_SERVER['HTTP_X_REQUESTED_WITH']) || $_SERVER['HTTP_X_REQUESTED_WITH'] !== 'XMLHttpRequest'){
         throw new exception('Yielding result only works with XHR!');
     }else{
+        if (!headers_sent()){
+            header("Content-Type: application/octet-stream");
+        }
         @ob_end_flush();
         fflush(fopen('php://stdout', 'r'));
         flush();
