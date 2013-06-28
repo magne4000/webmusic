3c9ff2f605c84f9c254a02603a6f8045c8fbcc0f
Joël Charles <joel.charles91@gmail.com>
2013-06-28 16:20:49 +0200

    Use jQuery.extend to follow jQuery best practises

--- plugins/hash/js/hash.js
+++ plugins/hash/js/hash.js
@@ -133,19 +133,27 @@
             return this;
         }
     };
-
-    $.hash = function(method) {
-        // Method calling logic
-        if (methods[method]) {
-            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-        } else if (typeof method === 'object' || !method) {
-            return methods.init.apply(this, arguments);
-        } else {
-            $.error('jQuery.hashChanged: Method ' + method + ' does not exist');
+    
+    $.extend({
+        hash: function(method) {
+            // Method calling logic
+            if (methods[method]) {
+                return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+            } else if (typeof method === 'object' || !method) {
+                return methods.init.apply(this, arguments);
+            } else {
+                $.error('jQuery.hashChanged: Method ' + method + ' does not exist');
+            }
         }
-    };
+    });
 })(jQuery);
 $(document).ready(function() {
     $.hash();
-    $.hash('changed');
+    if (ulMenu.loaded){
+        $.hash('changed');
+    }else{
+        $(document).one('ajaxmenuloaded', function(){
+            $.hash('changed');
+        });
+    }
 });
